ab2cd8ca2e87541e266836c137f46069335619b0
Guilherme Landim-Santos <62408637+guilherber@users.noreply.github.com>
2024-06-21 21:39:58 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,16 +3,18 @@ from setuptools import setup, find_packages
 setup(
     name='pythermalgreen',
     version='0.1',
+    author='Guilherme Landim-Santos',
+    author_email='guisaan.gui@gmail.com',
     packages=find_packages(),
     include_package_data=True,
     package_data={
         'pythermalgreen': ['config.yaml'],
     },
     install_requires=[
-        'numpy>=1.22.0',     
+        'numpy>=1.22.0',
         'rasterio==1.3.10',
         'geopandas==0.14.4',
-        'pandas<=2.2.2',       
-        'shapely==1.7.1'     
+        'pandas<=2.2.2',
+        'shapely==1.7.1'
     ],
 )
