e0564c9b6a7ab684cebb0c1e4181b4683e58ea40
Guilherme Landim-Santos <62408637+guilherber@users.noreply.github.com>
2024-06-21 21:27:47 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-  name = pythermalgreen,
-  version = '0.1',
-  packages = find_packages(),
-  install_requires = [
-
-  ],
+    name='pythermalgreen',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'pythermalgreen': ['config.yaml'],
+    },
+    install_requires=[
+        'numpy>=1.22.0',     
+        'rasterio==1.3.10',
+        'geopandas==0.14.4',
+        'pandas<=2.2.2',       
+        'shapely==1.7.1'     
+    ],
 )
