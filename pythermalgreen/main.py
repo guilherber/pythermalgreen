cf3bf842f1ed36d3f87172796e1bb4efa37f459c
Guilherme Landim-Santos <62408637+guilherber@users.noreply.github.com>
2024-06-25 09:07:25 -0300

    Add files via upload

--- pythermalgreen/main.py
+++ pythermalgreen/main.py
@@ -6,6 +6,8 @@ from rasterio.plot import show
 import pandas as pd
 import geopandas as gpd
 import numpy as np
+import pylandstats as pls
+import warnings
 
 class pythermalgreen:
     def __init__(self, tst_file, ndvi_file, output):
@@ -34,7 +36,7 @@ class pythermalgreen:
                 zs = src.read(1)
                 mask = src.read_masks(1) > 0
                 xs, ys, zs = xs[mask], ys[mask], zs[mask]
-        
+
         data = {"X": pd.Series(xs.ravel()),
                 "Y": pd.Series(ys.ravel())}
 
@@ -49,7 +51,7 @@ class pythermalgreen:
         quartis_temperatura = np.percentile(df['TST'], [25, 50, 75])
 
         def condicao_alta_temperatura_e_baixo_ndvi(temperatura, ndvi):
-            if temperatura >= quartis_temperatura[2] and ndvi < 0.3:
+            if temperatura >= quartis_temperatura[2] and ndvi < 0.5:
                 return 1
             else:
                 return 0
@@ -62,13 +64,13 @@ class pythermalgreen:
             transform = src.transform
             crs = src.crs
             raster = src.read(1, masked=True)
-        
+
         geometry = [Point(xy) for xy in zip(df.X, df.Y)]
         df8 = df.drop(['X', 'Y'], axis=1)
         gdf = GeoDataFrame(df8, crs=crs, geometry=geometry)
         geom = gdf['geometry']
         geom_value = [(geom, value) for geom, value in zip(gdf.geometry, gdf['Condicao'])]
-        
+
         rasterized = rasterize(
             geom_value,
             out_shape=raster.shape,
@@ -76,18 +78,48 @@ class pythermalgreen:
             all_touched=True,
             dtype=rio.float32
         )
-        
+
         filtered_rasterized = np.where(rasterized == 0, np.nan, rasterized)
-        
+
         with rio.open(self.output, "w",
-                     driver="GTiff",
-                     transform=transform,
-                     dtype=rio.float32,
-                     count=1,
-                     crs=crs,
-                     width=src.width,
-                     height=src.height) as dst:
+                      driver="GTiff",
+                      transform=transform,
+                      dtype=rio.float32,
+                      count=1,
+                      crs=crs,
+                      width=src.width,
+                      height=src.height) as dst:
             dst.write(filtered_rasterized, indexes=1)
-        
-        raster = rio.open(self.output)
-        show(raster)
+
+    def metrics(self):
+        try:
+            warnings.filterwarnings("ignore", category=RuntimeWarning)
+            warnings.filterwarnings("ignore", message="Module .* has not been compiled for Transonic-Numba")
+            raster = rio.open(self.output)
+            show(raster)
+            data = raster.read(1, masked=True)
+
+            unique_values = np.unique(data.compressed())
+            if len(unique_values) <= 1:
+                print(
+                    f"The raster contains only a single class (value {unique_values[0]}). No meaningful metrics can be computed.")
+                return
+            ls = pls.Landscape(self.output)
+            ls.nodata = 0  
+            patch_metrics_df = ls.compute_landscape_metrics_df()
+            if patch_metrics_df.empty:
+                print(
+                    "The DataFrame is empty. This is likely due to the raster containing insufficient class diversity.")
+            else:
+                with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+                    print(patch_metrics_df)
+
+        except ValueError as e:
+            print(f"ValueError: {e}")
+        except RuntimeWarning as e:
+            print(f"RuntimeWarning: {e}")
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+
+        finally:
+            warnings.resetwarnings()
